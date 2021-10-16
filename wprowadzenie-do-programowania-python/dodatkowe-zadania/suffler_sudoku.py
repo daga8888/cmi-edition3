b071545cb1c337262b7e86b357c7d836aad35677
PiotrMaciejKowalski <piotr.m.kowalski86@gmail.com>
2021-10-16 23:21:38 +0200

    trzecia iteracja implementacji dodanie wkładania 100% wyznaczen

--- wprowadzenie-do-programowania-python/dodatkowe-zadania/suffler_sudoku.py
+++ wprowadzenie-do-programowania-python/dodatkowe-zadania/suffler_sudoku.py
@@ -28,8 +28,19 @@ def print_sudoku(sudoku):
         if i % 3 == 2:
             print("---+---+---")
 
+def print_hints(hints):
+    for i in range(9):
+        for j in range(9):
+            print('[',sep='',end='')
+            for k in range(1,10):
+                if str(k) in hints[i][j]:
+                    print(k,sep='',end='')
+                else:
+                    print(' ', sep='', end='')
+            print(']',sep='', end='')
+        print('',sep='',end='\n')
+
 def check(sudoku_part):
-    #sudoku_part = flatten(sudoku_part)
     is_okey = True
     for i in range(1,9):
         count = sum([element for element in sudoku_part if element == i])
@@ -59,32 +70,42 @@ def is_solved(sudoku):
     list_of_Nones = [None for i in range(9) for j in range(9) if sudoku[i][j] is None]
     return len(list_of_Nones) == 0
 
+import copy
+
 def generate_hints(sudoku):
+    hints = copy.deepcopy(sudoku)
+    for i in range(9):
+        for j in range(9):
+            if sudoku[i][j] is None:
+                hint = {str(i) for i in range(1,10)}
+                row = {element for element in sudoku[i] if element is not None}
+                col = {element for element in [sudoku[x][j] for x in range(9)] if element is not None }
+                sqr = {element for element in [sudoku[x][y] for x in range(3) if x//3 == i//3 for y in range(9) if y//3 == j//3] if element is not None}
+                result =  hint.difference(row).difference(col).difference(sqr)
+                hints[i][j] = result
+    return hints
 
+def insert_values(hints):
+    return [ [ hints[i][j] if isinstance(hints[i][j],str) else hints[i][j].pop() if len(hints[i][j]) == 1 else None for j in range(9)] for i in range(9)]
     pass
 
-
-
-sudoku = parse(sudoku_str)
-print(is_solved(sudoku))
-
-def generate_inserts(hints):
-    pass
-
-def insert_values(sudoku, inserts):
-    pass
-
-def solve_sudoku(sudoku_string : str):
+def solve_sudoku(sudoku_string : str, max_steps = 20):
     # konwersja na wewnetrzny typ sudoku
     sudoku = parse(sudoku_string)
-
+    step = 0
     while not is_solved(sudoku):
+        step += 1
         ## generujemy podpowiedzi
         ## na ich podstawie okreslamy liczby do wstawienia w tej iteracji
         ## i wstawiamy wartości do sudoku
         hints = generate_hints(sudoku)
-        inserts = generate_inserts(hints)
-        sudoku = insert_values(sudoku, inserts)
+        sudoku = insert_values(hints)
         if not is_correct(sudoku):
-            return "Err 1", sudoku
-    return sudoku
+            return sudoku, hints
+        if step >= max_steps:
+            return sudoku, hints
+    return sudoku, hints
+
+sudoku, hints = solve_sudoku(sudoku_str)
+print_sudoku(sudoku)
+print_hints(hints)

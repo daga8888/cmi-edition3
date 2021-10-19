8359f4b2a5ceebcdb2f31757705d34cef5a415ea
PiotrMaciejKowalski <piotr.m.kowalski86@gmail.com>
2021-10-19 22:42:10 +0200

    zakonczenie prac nad biblioteka i stworzenie runera dla kodu

--- wprowadzenie-do-programowania-python/dodatkowe-zadania/suffler_sudoku.py
+++ wprowadzenie-do-programowania-python/dodatkowe-zadania/suffler_sudoku.py
@@ -1,17 +1,15 @@
-sudoku_str = """
-005000048
-020000001
-800410000
-600702030
-004900000
-200000009
-000001060
-000350704
-080000000
-"""
+import copy
+from itertools import chain, combinations
+
 
 
 def parse(sudoku_string : str):
+    """
+    celem funkcji jest przyjecie wielolinijkowego sudoku w postaci 9 linijek postaci
+    123000789,
+    gdzie liczby oznaczaja konkretne wartosc a 0 wartosci puste
+    zwracana jest wewnetrzna reprezentacja takiego sudoku jako tablica 2 wymiarowa
+    """
     lines = sudoku_string.strip().replace(' ', '').split('\n')
     sudoku = []
     for line in lines:
@@ -20,6 +18,9 @@ def parse(sudoku_string : str):
 
 
 def print_sudoku(sudoku):
+    """
+    powoduje wypisanie na ekranie obiektu sudoku w reprezentacji pakietu
+    """
     for i in range(9):
         for j in range(9):
             if sudoku[i][j] is not None:
@@ -34,6 +35,10 @@ def print_sudoku(sudoku):
 
 
 def print_hints(hints):
+    """
+    jak print_sudoku ale tu wypisany jest obiekt hintsow - opisu pol i dostepnych dla nich wartosci
+    czyli takich ktore nie naurszaja porzadku wierszy, kolumn i kwadratow
+    """
     for i in range(9):
         for j in range(9):
             print('[',sep='',end='')
@@ -47,6 +52,9 @@ def print_hints(hints):
 
 
 def check(sudoku_part):
+    """
+    fukcja do sprawdzania czy dana linia lub kwadrat nie posiadaja wiecej niz 1 takiej samej wartosci
+    """
     is_okey = True
     for i in range(1,9):
         count = sum([element for element in sudoku_part if element == i])
@@ -57,6 +65,9 @@ def check(sudoku_part):
 
 
 def is_correct(sudoku):
+    """
+    funkcja sprawdza czy obiekt sudoku nie jest sprzeczny (te same elementy na wykluczajacych polach)
+    """
     # sprawdz linie
     is_okey = True
     for i in range(9):
@@ -72,32 +83,37 @@ def is_correct(sudoku):
 
 
 def is_solved(sudoku):
+    """
+    funkcja sprawdza czy sudoku nie jest juz wypelnione
+    """
     is_okey = is_correct(sudoku)
     if not is_okey:
         return False
     list_of_Nones = [None for i in range(9) for j in range(9) if sudoku[i][j] is None]
     return len(list_of_Nones) == 0
 
-import copy
-
-# Ta funkcja ma wygenerowac wszystkie dopuszczalne liczby dla pol pustych
-# To znaczy ze w danym polu moze byc kazda z liczb 1..9 za wyjatkiem tej ktora juz wystepuje w rzedzie, kolumnie lub
-# kwadracie
-
 
 def get_row(sudoku, row):
+    """zwraca odpowiedni wiersz z sudoku"""
     return sudoku[row]
 
 
 def get_col(sudoku, col):
+    """zwraca odpowiednia kolumne z sudoku"""
     return [sudoku[x][col] for x in range(9)]
 
 
 def get_sqr(sudoku, row, col):
+    """zwraca kwadrat 3x3 zawierajacy pole (row,col)"""
     return [sudoku[x][y] for x in range(9) if x//3 == row//3 for y in range(9) if y//3 == col//3]
 
 
 def generate_hints(sudoku):
+    """
+    Ta funkcja ma wygenerowac wszystkie dopuszczalne liczby dla pol pustych
+    To znaczy ze w danym polu moze byc kazda z liczb 1..9 za wyjatkiem tej ktora juz wystepuje w rzedzie, kolumnie lub
+    kwadracie. Sa to tzw hints lub olowek
+    """
     hints = copy.deepcopy(sudoku)
     for i in range(9):
         for j in range(9):
@@ -114,6 +130,7 @@ def generate_hints(sudoku):
 
 
 def get_neighbours(sudoku, row, col):
+    """Funkcja zwraca 3 listy sasiadow danego pola, czyli np. wiersz tego pola, ale bez samego pola"""
     row_neighbours = [sudoku[row][y] for y in range(9) if y != col]
     col_neighbours = [sudoku[x][col] for x in range(9) if x != row]
     sqr_neighbours = [sudoku[x][y] for x in range(9) if x//3 == row//3 for y in range(9) if y//3 == col//3 if x!=row or y!=col]
@@ -121,6 +138,7 @@ def get_neighbours(sudoku, row, col):
 
 
 def union(arr):
+    """arr jest lista obiektow zbiorow (set). Funkcja zwraca ich laczna sume mnogosciowa"""
     return set().union(*arr)
 
 
@@ -139,12 +157,86 @@ def check_unique_values(hints):
     return hints
 
 
+
+
+def powerset(iterable):
+    """powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"""
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+def search_cycles(hints):
+    """
+    Metoda poszukuje cykli w hintach np trzech pól {2,3,4}. Wtedy można poznać ktore elementy nie znajduja w
+    pozostalych hintach.
+    Warunkiem do sprawdzenia jest czy suma mnogosciowa n pol ma n roznych wartosci.
+    Problemem jest jednak przejrzenie wszystkich wyborow elementow do cyklu ktorych jest 2^9 dla kazdego wiersza kolumny
+    Drugim problemem jest usuniecie wykrytego cyklu z hintow
+    """
+    permutation_source = [x for x in range(9)]
+
+    for row in range(9):
+        for combination in powerset(permutation_source):
+            if len(combination) == 0 or len(combination) == 1:
+                continue
+            # ignorujmy combinacje gdzie są 1elementowe hinty
+            lengths = [ len(hints[row][x]) for x in combination]
+            if 1 in lengths:
+                continue
+            # sprawdzamy warunek cyklu
+            cycle_union = union([hints[row][x] for x in combination])
+            if len(cycle_union) != len(combination):
+                continue
+            # usuwamy cykl z innych elementow wiersza
+            for y in range(9):
+                if y not in combination:
+                    hints[row][y] = hints[row][y].difference(cycle_union)
+    for column in range(9):
+        for combination in powerset(permutation_source):
+            if len(combination) == 0 or len(combination) == 1:
+                continue
+            # ignorujmy combinacje gdzie są 1elementowe hinty
+            lengths = [ len(hints[x][column]) for x in combination]
+            if 1 in lengths:
+                continue
+            # sprawdzamy warunek cyklu
+            cycle_union = union([hints[x][column] for x in combination])
+            if len(cycle_union) != len(combination):
+                continue
+            # usuwamy cykl z innych elementow wiersza
+            for y in range(9):
+                if y not in combination:
+                    hints[y][column] = hints[y][column].difference(cycle_union)
+        for row in range(0,9,3):
+            for column in range(0,9,3):
+                index_set =  [(r, c) for r in range(row, row + 3) for c in range(column, column + 3)]
+                for combination in powerset(index_set):
+                    if len(combination) == 0 or len(combination) == 1:
+                        continue
+                    # ignorujmy combinacje gdzie są 1elementowe hinty
+                    lengths = [len(hints[x][y]) for x, y in combination]
+                    if 1 in lengths:
+                        continue
+                    # sprawdzamy warunek cyklu
+                    cycle_union = union([hints[x][y] for x, y in combination])
+                    if len(cycle_union) != len(combination):
+                        continue
+                    # usuwamy cykl z innych elementow wiersza
+                    for x,y in index_set:
+                        if (x,y) not in combination:
+                            hints[x][y] = hints[x][y].difference(cycle_union)
+    return hints
+
+
 def insert_values(hints):
+    """Funkcja generuje stan rozwiazania sudoku na podstawie hints wstawiajac wartosci gdzie tylko jest to pewne"""
     hints = copy.deepcopy(hints)
     return [ [ hints[i][j].pop() if len(hints[i][j]) == 1 else None for j in range(9)] for i in range(9)]
 
 
-def solve_sudoku(sudoku_string : str, max_steps = 10):
+def solve_sudoku(sudoku_string : str, max_steps = 50):
+    """Funkcja poszukujaca rozwiazania sudoku przez okreslona liczbe iteracji.
+    Na wejsciu przyjmuje sudoku w postaci tekstu, konwersja na obiekt sudoku nastepuje
+    juz we wnetrzu kodu"""
     # konwersja na wewnetrzny typ sudoku
     sudoku = parse(sudoku_string)
     step = 0
@@ -155,11 +247,10 @@ def solve_sudoku(sudoku_string : str, max_steps = 10):
         ## i wstawiamy wartości do sudoku
         hints = generate_hints(sudoku)
         ## na podstawie hintsow mozemy sprawdzic juz wiele wskazowek do wstawienia wartosci
-        print(f'Hinty wygenerowane poczatkowo')
-        print_hints(hints)
-        print(f'Hinty uzyskane z unikalnych')
+        hints = search_cycles(hints)
+        # cykle nalezy sprawdzic pierwsze - bo choc trudniejsze obliczeniowo, to dzieki nim moze uda sie wiecej
+        # unikatow wstawic do tresci
         hints = check_unique_values(hints)
-        print_hints(hints)
         ## wygenerowanie sudoku na podstawie hints
         sudoku = insert_values(hints)
         if not is_correct(sudoku):
@@ -170,8 +261,3 @@ def solve_sudoku(sudoku_string : str, max_steps = 10):
             return sudoku, hints
     return sudoku, hints
 
-
-sudoku, hints = solve_sudoku(sudoku_str)
-print_sudoku(sudoku)
-print_hints(hints)
-print('Good bye')

0402f750cb31f55dd41a2612bc25c8cb4dff0e10
PiotrMaciejKowalski <piotr.m.kowalski86@gmail.com>
2021-11-06 08:40:37 +0100

    dodanie dynamiki spotkan

--- wprowadzenie-do-programowania-python/part6/pandemia_dzumdzumy.py
+++ wprowadzenie-do-programowania-python/part6/pandemia_dzumdzumy.py
@@ -1,4 +1,4 @@
-from random import random
+from random import random, randrange, sample
 
 class Choroba(object):
 
@@ -31,22 +31,25 @@ class Czlowiek(object):
 
     def mija_dzien(self):
         if self.choroba is not None and self.choroba.czy_koniec():
+            self.odpornosc = 100
             self.choroba = None
         if self.choroba is None:
             self.zdrowie +=1
         else:
             self.zdrowie -=5
-        self.odpornosc -= 1
+        if self.odpornosc > 0:
+            self.odpornosc -= 1
 
 
 class Swiat(object):
 
     # funkcja stworzenia swiata
-    def __init__(self, liczba_ludzi = 0, liczba_chorych = 0):
+    def __init__(self, liczba_ludzi = 0, liczba_chorych = 0, licza_spotkan_kazdego_czlowieka = (0,10)):
         self.rok=2020
         self.dzien=0
         self.populacja = []
         self.choroby = []
+        self.liczba_spotkan_kazdego_czlowieka = licza_spotkan_kazdego_czlowieka
         for _ in range(liczba_chorych):
             nowy = Czlowiek()
             nowa_choroba = Choroba()
@@ -60,12 +63,21 @@ class Swiat(object):
     def wypisz_stan_pandemii(self):
         liczba_chorych = len([czlowiek for czlowiek in self.populacja if czlowiek.czy_chory()])
         liczba_ludzi = len(self.populacja)
-        print(f'Populacja liczy {liczba_ludzi} osób z których chorych jest {liczba_chorych}', end=' ')
-        print(f'Stanowi to {liczba_chorych/liczba_ludzi * 100}% populacji')
+        print(f'Populacja liczy {liczba_ludzi} osób z których chorych jest {liczba_chorych:5}', end=' ')
+        print(f'Stanowi to {int(liczba_chorych/liczba_ludzi * 100):3}% populacji', end=' ')
+        print(f'Przeciętna odporność wynosi {sum([int(czlowiek.odpornosc) for czlowiek in self.populacja])/len(self.populacja):3}%')
 
     def mija_dzien(self):
         # co sie dzieje codziennie na swiecie
         print(f'Mamy dzień {self.dzien:3} roku {self.rok}', end=' ')
+        ## kontakty miedzy ludzkie
+        min, maks = self.liczba_spotkan_kazdego_czlowieka
+        for czlowiek in self.populacja:
+            ile_spotkan = randrange(min, maks)
+            for drugi_czlowiek in sample(self.populacja, ile_spotkan):
+                czlowiek.kontakt(drugi_czlowiek)
+                drugi_czlowiek.kontakt(czlowiek)
+        ##
         for czlowiek in self.populacja:
             czlowiek.mija_dzien()
         self.choroby = [ czlowiek.choroba for czlowiek in self.populacja if czlowiek.czy_chory()]
@@ -82,5 +94,5 @@ class Swiat(object):
         for _ in range(liczba_dni):
             self.mija_dzien()
 
-swiat = Swiat(liczba_ludzi=10000, liczba_chorych=1)
-swiat.uruchom_przez(30)
+swiat = Swiat(liczba_ludzi=10000, liczba_chorych=1, licza_spotkan_kazdego_czlowieka=(0,4))
+swiat.uruchom_przez(365)
